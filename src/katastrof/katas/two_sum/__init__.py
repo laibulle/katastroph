eb06297cc244f1b92ced36dc330b8fefa6ae7c82
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-02 20:37:39 +0200

    first commit

--- src/katastrof/katas/two_sum/__init__.py
+++ src/katastrof/katas/two_sum/__init__.py
@@ -2,4 +2,12 @@ from typing import Iterable
 
 
 def two_sum(numbers: Iterable[int], target: int) -> tuple[int, int] | None:
-    raise NotImplementedError
+    i = 0
+    last_idx_to_test = len(numbers) - 1
+
+    while i < last_idx_to_test:
+        for op2 in numbers[i+1::]:
+            if (numbers[i] + op2) == target:
+                return (numbers[i], op2)
+        i += 1
+    return None
