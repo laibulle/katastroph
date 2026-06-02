486313f35741454bcb536f2c086a7732286d9d88
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-02 19:19:35 +0200

    first commit

--- src/katastrof/katas/string_analysis/__init__.py
+++ src/katastrof/katas/string_analysis/__init__.py
@@ -1,5 +1,8 @@
+from functools import reduce
+
+
 def char_frequencies(text: str) -> dict[str, int]:
-    raise NotImplementedError
+    return reduce(lambda acc, item: acc | {item: acc.get(item, 0) + 1}, text, {})
 
 
 def is_anagram(left: str, right: str) -> bool:
