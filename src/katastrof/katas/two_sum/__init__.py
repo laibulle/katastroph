54edafd32d2738e9b01eb1cfd10af17ca542775b
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-02 20:40:23 +0200

    first commit

--- src/katastrof/katas/two_sum/__init__.py
+++ src/katastrof/katas/two_sum/__init__.py
@@ -7,7 +7,15 @@ def two_sum(numbers: Iterable[int], target: int) -> tuple[int, int] | None:
 
     while i < last_idx_to_test:
         for op2 in numbers[i+1::]:
-            if (numbers[i] + op2) == target:
-                return (numbers[i], op2)
+            res = equals_target(numbers[i], op2, target)
+            if res is not None:
+                return res
         i += 1
     return None
+
+
+def equals_target(op1, op2, target) -> tuple[int, int] | None:
+    if op1 + op2 == target:
+        return (op1, op2)
+    else:
+        None
