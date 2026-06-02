7c2cdbf64a037e379292f014324a4e6ee45af227
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-03 00:10:32 +0200

    first commit

--- src/katastrof/katas/fibonacci/__init__.py
+++ src/katastrof/katas/fibonacci/__init__.py
@@ -1,2 +1,18 @@
 def fibonacci_iterative(n: int) -> int:
-    raise NotImplementedError
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    i = 2
+    i_1 = 1
+    i_2 = 0
+
+    while i <= n:
+        t_1 = i_1 + i_2
+        i_1 = i_2
+        i_2 = t_1
+        i+=1
+
+    return i_2 + i_1
+
