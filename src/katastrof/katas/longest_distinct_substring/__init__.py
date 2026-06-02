bdffa8d604ec2d378b51b1f35d86da7a2c9a8f83
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-03 01:05:22 +0200

    first commit

--- src/katastrof/katas/longest_distinct_substring/__init__.py
+++ src/katastrof/katas/longest_distinct_substring/__init__.py
@@ -1,2 +1,21 @@
+from operator import contains
+
+
 def longest_distinct_substring(text: str) -> str:
-    raise NotImplementedError
+    sub = ""
+    current = ""
+    for char in text:
+        if len(current) > 0 and contains(current, char):
+            if len(current) > len(sub):
+                sub = current
+            current = ""
+        current += char
+
+    return sub
+
+
+
+
+
+
+
