4303e9da4f9f9faafa46adb99f07fe45375824c0
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-03 00:15:31 +0200

    first commit

--- src/katastrof/katas/fibonacci/__init__.py
+++ src/katastrof/katas/fibonacci/__init__.py
@@ -1,10 +1,10 @@
-def fibonacci_iterative(n: int) -> ValueError | int:
+def fibonacci_iterative(n: int) -> int:
     if n == 0:
         return 0
     elif n == 1:
         return 1
     elif n < 0:
-        return ValueError(f"{n} is negative")
+        raise ValueError(f"{n} is negative")
 
     i, i_1, i_2 = 2, 1, 0
 
