25f16643694e266703ee517601b5d3383b2ca939
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-06-03 13:39:36 +0200

    first commit

--- src/katastrof/katas/salut_toto/__init__.py
+++ src/katastrof/katas/salut_toto/__init__.py
@@ -1,2 +1,51 @@
+
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+SALUT_STRING = "Salut "
+TOTO_STRING = "Toto "
+
 def alternating_salut_toto(repetitions: int) -> str:
-    raise NotImplementedError
+    printer = SalutTotoPrinter(repetitions)
+    printer.start()
+    return printer.res[::-1]
+
+
+class SalutTotoPrinter:
+    def __init__(self, repetitions: int):
+        self.thread_lock = threading.Lock()
+        self.res = ""
+        self.next_value = SALUT_STRING
+        self.current_index = 0
+        self.repetitions = repetitions
+
+    def start(self):
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(self.print_salut_thread)
+            executor.submit(self.print_toto_thread)
+
+    def set_next_print(self) -> None:
+        self.thread_lock.acquire()
+        self.res = self.res + self.next_value
+        print(self.res)
+        if self.next_value == SALUT_STRING:
+            self.next_value = TOTO_STRING
+            self.current_index += 1
+        else:
+            self.next_value = SALUT_STRING
+        self.thread_lock.release()
+
+    def print_salut_thread(self) -> None:
+        while self.not_finished():
+            if self.next_value == SALUT_STRING:
+                self.set_next_print()
+
+    def print_toto_thread(self) -> None:
+        while self.not_finished():
+            if self.next_value == TOTO_STRING:
+                self.set_next_print()
+
+    def not_finished(self) -> bool:
+        return self.current_index < self.repetitions
+
+
--- src/katastrof/katas/tail/__init__.py
+++ src/katastrof/katas/tail/__init__.py
@@ -1,2 +1,20 @@
+LF = '\n'
+
 def tail_lines(text: str, count: int) -> list[str]:
-    raise NotImplementedError
+    current_count = count
+    current_index = len(text) - 1
+    latest_index = current_index
+    lines = []
+
+    if text[current_index] == LF:
+        latest_index += 1
+
+    while current_count > 0 and current_index > 0:
+        if text[current_index] == LF:
+            lines.insert(0, text[current_index-1:latest_index-1])
+            latest_index = current_index - 1
+            current_count -= 1
+
+        current_index -= 1
+
+    return lines
